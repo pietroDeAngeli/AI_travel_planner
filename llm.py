a998f8be9c384292fbcf20d1871eff40f4f1b5aa
pietroDeAngeli <pietro.deangeli@studenti.unitn.it>
2026-01-05 18:31:29 +0100

    Initial commit

--- llm.py
+++ llm.py
@@ -4,7 +4,6 @@ from transformers import pipeline
 MODEL_ID = "meta-llama/Meta-Llama-3.1-8B-Instruct"
 
 def make_llm(model_id: str = MODEL_ID):
-    # In Colab spesso bfloat16 va bene se la GPU lo supporta, altrimenti float16
     dtype = torch.bfloat16 if torch.cuda.is_available() else torch.float32
 
     pipe = pipeline(
@@ -12,6 +11,7 @@ def make_llm(model_id: str = MODEL_ID):
         model=model_id,
         model_kwargs={"torch_dtype": dtype},
         device_map="auto",
+        #verbose=False,
     )
     return pipe
 
