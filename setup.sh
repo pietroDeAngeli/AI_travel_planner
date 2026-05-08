ae24dd4a4c7c9c392523d47bf764726ac7a45529
pietroDeAngeli <pietro.deangeli@studenti.unitn.it>
2026-05-08 14:47:01 +0200

    setup updated

--- setup.sh
+++ setup.sh
@@ -4,4 +4,3 @@ source .venv/bin/activate
 pip install --upgrade pip
 pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu121
 pip install -r requirements.txt
-python app.py
